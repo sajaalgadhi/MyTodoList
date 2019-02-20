a49c05c43a501d3afff53e58383425a8b3028625
sajaalgadhi <saja.algadhi@outlook.com>
2019-02-20 16:06:41 +0300

    Updates

---
+++ precache-manifest.4c728331ace9582c973dbfd3fbe32c00.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "cec4bad3555c33df0e95",
+    "url": "//MyTodoList/static/css/main.042c15de.chunk.css"
+  },
+  {
+    "revision": "cec4bad3555c33df0e95",
+    "url": "//MyTodoList/static/js/main.cec4bad3.chunk.js"
+  },
+  {
+    "revision": "82a5a6e2613b03cf19df",
+    "url": "//MyTodoList/static/js/runtime~main.82a5a6e2.js"
+  },
+  {
+    "revision": "23ba732b6b463f1ff3e5",
+    "url": "//MyTodoList/static/js/2.23ba732b.chunk.js"
+  },
+  {
+    "revision": "cc0dd4b5f4ce1a745d5505c6de0c8f60",
+    "url": "//MyTodoList/static/media/thumb-1920-651074.cc0dd4b5.jpg"
+  },
+  {
+    "revision": "4aa17c892619fa2ab81a8edebf5bbf51",
+    "url": "//MyTodoList/index.html"
+  }
+];
